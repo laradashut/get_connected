4a63437f6ef0219a4fe5464b2f36bab638ba81c5
laradashut <lara.dashut@gmail.com>
2018-06-27 13:49:44 -0700

    backend is ready

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/get_connected", {
-    keepAlive: true,
-    useMongoClient: true
+mongoose.connect("mongodb://localhost/warbler", {
+    keepAlive: true
 });
 
-
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -14,12 +14,40 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true 
+        required: true
     },
     profileImageUrl: {
-        type: String 
+        type: String
+    },
+    messages: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message"
+        }
+    ]
+});
+
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch (err) {
+        return next(err);
     }
 });
+
+userSchema.methods.comparePassword = async function (candidatePassword, next) {
+    try {
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        return next(err);
+    }
+};
 
 const User = mongoose.model("User", userSchema);
 
